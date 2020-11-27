9b281ea5b0649ccc93650a015b500da9143a9c97
Shinzo SAITO <architshin@websarva.com>
2020-11-28 03:37:05 +0900

    AsyncKotlinSampleをJava版のコードに合わせて改変。

--- AsyncKotlinSample/app/src/main/java/com/websarva/wings/android/asynckotlinsample/MainActivity.kt
+++ AsyncKotlinSample/app/src/main/java/com/websarva/wings/android/asynckotlinsample/MainActivity.kt
@@ -46,32 +46,19 @@ class MainActivity : AppCompatActivity() {
 		 * お天気APIにアクセスすするためのAPI Key。
 		 * ※※※※※この値は各自のものに書き換える!!※※※※※
 		 */
-		private const val APP_ID = "913136635cfa3182bbe18e34ffd44849"
+		private const val APP_ID = ""
 	}
 
 	/**
 	 * リストビューに表示させるリストデータ。
 	 */
-	private val _list: MutableList<MutableMap<String, String>> = mutableListOf()
+	private var _list: MutableList<MutableMap<String, String>> = mutableListOf()
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
-		var city = mutableMapOf("name" to "大阪", "q" to "Osaka")
-		_list.add(city)
-		city = mutableMapOf("name" to "神戸", "q" to "Kobe")
-		_list.add(city)
-		city = mutableMapOf("name" to "京都", "q" to "Kyoto")
-		_list.add(city)
-		city = mutableMapOf("name" to "大津", "q" to "Otsu")
-		_list.add(city)
-		city = mutableMapOf("name" to "奈良", "q" to "Nara")
-		_list.add(city)
-		city = mutableMapOf("name" to "和歌山", "q" to "Wakayama")
-		_list.add(city)
-		city = mutableMapOf("name" to "姫路", "q" to "Himeji")
-		_list.add(city)
+		_list = createList()
 
 		val lvCityList = findViewById<ListView>(R.id.lvCityList)
 		val from  = arrayOf("name")
@@ -82,17 +69,29 @@ class MainActivity : AppCompatActivity() {
 	}
 
 	/**
-	 * 取得したお天気情報を画面に表示するメソッド。
+	 * リストビューに表示させる天気ポイントリストデータを生成するメソッド。
 	 *
-	 * @param telop お天気情報の表題。
-	 * @param desc お天気情報の内容。
+	 * @return 生成された天気ポイントリストデータ。
 	 */
-	@UiThread
-	private fun showResult(telop: String, desc: String) {
-		val tvWeatherTelop = findViewById<TextView>(R.id.tvWeatherTelop)
-		val tvWeatherDesc = findViewById<TextView>(R.id.tvWeatherDesc)
-		tvWeatherTelop.text = telop
-		tvWeatherDesc.text = desc
+	private fun createList(): MutableList<MutableMap<String, String>> {
+		var list: MutableList<MutableMap<String, String>> = mutableListOf()
+
+		var city = mutableMapOf("name" to "大阪", "q" to "Osaka")
+		list.add(city)
+		city = mutableMapOf("name" to "神戸", "q" to "Kobe")
+		list.add(city)
+		city = mutableMapOf("name" to "京都", "q" to "Kyoto")
+		list.add(city)
+		city = mutableMapOf("name" to "大津", "q" to "Otsu")
+		list.add(city)
+		city = mutableMapOf("name" to "奈良", "q" to "Nara")
+		list.add(city)
+		city = mutableMapOf("name" to "和歌山", "q" to "Wakayama")
+		list.add(city)
+		city = mutableMapOf("name" to "姫路", "q" to "Himeji")
+		list.add(city)
+
+		return list;
 	}
 
 	/**
@@ -103,70 +102,54 @@ class MainActivity : AppCompatActivity() {
 			val item = _list.get(position)
 			val q = item.get("q")
 			q?.let {
-				val receiver = WeatherInfoReceiver()
-				receiver.execute(WEATHERINFO_URL, it, APP_ID)
+				val url = "$WEATHERINFO_URL&q=$q&appid=$APP_ID"
+				asyncExecute(url)
 			}
 		}
 	}
 
 	/**
-	 * Web APIにアクセスしてお天気情報を取得するクラス。
+	 * お天気情報の取得処理を行うメソッド。
+	 *
+	 * @param url お天気情報を取得するURL。
 	 */
-	private inner class WeatherInfoReceiver {
-		/**
-		 * お天気情報の取得処理を行うメソッド。
-		 *
-		 * @param urlBase お天気情報のWeb APIの規定となるURL。
-		 * @param q お天気情報を取得する対象となる都市情報。
-		 * @param appId お天気APIにアクセスするためのAPIキー。
-		 */
-		@UiThread
-		fun execute(urlBase: String, q: String, appId: String) {
-			val handler = HandlerCompat.createAsync(mainLooper)
-			val backgroundReceiver = WeatherInfoBackgroundReceiver(handler, urlBase, q, appId)
-			val executeService = Executors.newSingleThreadExecutor()
-			executeService.submit(backgroundReceiver)
-		}
+	@UiThread
+	fun asyncExecute(url: String) {
+		val handler = HandlerCompat.createAsync(mainLooper)
+		val backgroundTask = BackgroundTask(handler, url)
+		val executeService = Executors.newSingleThreadExecutor()
+		executeService.submit(backgroundTask)
 	}
 
 	/**
 	 * 非同期でお天気情報APIにアクセスするためのクラス。
 	 *
 	 * @param handler ハンドラオブジェクト。
-	 * @param urlBase お天気情報のWeb APIの規定となるURL。
-	 * @param q お天気情報を取得する対象となる都市情報。
-	 * @param appId お天気APIにアクセスするためのAPIキー。
+	 * @param url お天気情報を取得するURL。
 	 */
-	private inner class WeatherInfoBackgroundReceiver(handler: Handler, urlBase: String, q: String, appId: String): Runnable {
+	private inner class BackgroundTask(handler: Handler, url: String): Runnable {
 		/**
 		 * ハンドラオブジェクト。
 		 */
 		private val _handler = handler
 		/**
-		 * お天気情報を取得するURLの完全形。
+		 * お天気情報を取得するURL。
 		 */
-		private val _urlFull = "$urlBase&q=$q&appid=$appId"
+		private val _url = url
 
 		@WorkerThread
 		override fun run() {
 			var result = ""
-			try {
-				val url = URL(_urlFull)
-				val con = url.openConnection() as? HttpURLConnection
-				con?.run {
-					connectTimeout = 1000
-					readTimeout = 1000
-					requestMethod = "GET"
-					connect()
-					result = is2String(inputStream)
-					disconnect()
-					inputStream.close()
-				}
+			val url = URL(_url)
+			val con = url.openConnection() as? HttpURLConnection
+			con?.run {
+				requestMethod = "GET"
+				connect()
+				result = is2String(inputStream)
+				disconnect()
+				inputStream.close()
 			}
-			catch(ex: SocketTimeoutException) {
-				Log.e(DEBUG_TAG, "通信タイムアウト", ex)
-			}
-			val postExecutor = WeatherInfoPostExecutor(result)
+			val postExecutor = PostExecutor(result)
 			_handler.post(postExecutor)
 		}
 
@@ -194,7 +177,10 @@ class MainActivity : AppCompatActivity() {
 	 *
 	 * @param result Web APIから取得したお天気情報JSON文字列。
 	 */
-	private inner class WeatherInfoPostExecutor(result: String): Runnable {
+	private inner class PostExecutor(result: String): Runnable {
+		/**
+		 * 取得したお天気情報JSON文字列。
+		 */
 		private val _result = result
 
 		@UiThread
@@ -206,7 +192,11 @@ class MainActivity : AppCompatActivity() {
 			val description = weatherJSON.getString("description")
 			val telop = cityName + "の天気"
 			val desc = "現在は" + description + "です。"
-			showResult(telop, desc)
+
+			val tvWeatherTelop = findViewById<TextView>(R.id.tvWeatherTelop)
+			val tvWeatherDesc = findViewById<TextView>(R.id.tvWeatherDesc)
+			tvWeatherTelop.text = telop
+			tvWeatherDesc.text = desc
 		}
 	}
 }
