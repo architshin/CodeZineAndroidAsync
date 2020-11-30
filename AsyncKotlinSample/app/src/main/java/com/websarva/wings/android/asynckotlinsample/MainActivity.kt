9955f602e4556adab486a8f46f06edd6c46f57ce
Shinzo SAITO <architshin@websarva.com>
2020-11-30 19:20:10 +0900

    import文を最適化。

--- AsyncKotlinSample/app/src/main/java/com/websarva/wings/android/asynckotlinsample/MainActivity.kt
+++ AsyncKotlinSample/app/src/main/java/com/websarva/wings/android/asynckotlinsample/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.websarva.wings.android.asynckotlinsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ListView
@@ -11,15 +9,13 @@ import android.widget.SimpleAdapter
 import android.widget.TextView
 import androidx.annotation.UiThread
 import androidx.annotation.WorkerThread
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.HandlerCompat
 import org.json.JSONObject
 import java.io.BufferedReader
-import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
-import java.lang.StringBuilder
 import java.net.HttpURLConnection
-import java.net.SocketTimeoutException
 import java.net.URL
 import java.util.concurrent.Executors
 
